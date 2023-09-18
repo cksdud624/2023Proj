3fdd4e493b97f8569128989dc323d9ac07849f33
cksdud624 <cksdud624@naver.com>
2023-09-18 18:53:39 +0900

    20230918

--- Assets/Resources/Scripts/20230918/CharacterControl.cs
+++ Assets/Resources/Scripts/20230918/CharacterControl.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
     public float runSpeed = 10.0f;
     public float rotationSpeed = 360.0f;
+
+    public GameObject Sword;
+    public GameObject Arrow;
+    public GameObject Bow;
+
+    float subRunSpeed;
 
     CharacterController pcController;
     Animator animator;
@@ -14,6 +21,8 @@ public class CharacterControl : MonoBehaviour
     {
         pcController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+
+        subRunSpeed = runSpeed;
     }
     void Update()
     {
@@ -23,6 +32,18 @@ public class CharacterControl : MonoBehaviour
     void CharacterControl_Slerp()
     {
         direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            subRunSpeed = runSpeed / 2;
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+            subRunSpeed = runSpeed;
+        }
+
         if(direction.sqrMagnitude > 0.01f)
         {
             Vector3 forward = Vector3.Slerp(transform.forward, direction,
@@ -30,14 +51,53 @@ public class CharacterControl : MonoBehaviour
 
             transform.LookAt(transform.position + forward);
         }
-        else
-        {
-
-        }
 
 
         animator.SetFloat("Speed", pcController.velocity.magnitude);
-        pcController.Move(direction * runSpeed * Time.deltaTime + Physics.gravity * Time.deltaTime);
+        pcController.Move(direction * subRunSpeed * Time.deltaTime + Physics.gravity * Time.deltaTime);
+
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("Sword");
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+            animator.SetTrigger("Bow");
+        }
+
+        if (animator.GetCurrentAnimatorStateInfo(1).IsName("Sword"))
+        {
+            Sword.SetActive(true);
+        }
+        else
+        {
+            Sword.SetActive(false);
+        }
+
+        if (animator.GetCurrentAnimatorStateInfo(1).IsName("Bow"))
+        {
+            Bow.SetActive(true);
+
+            if (animator.GetCurrentAnimatorStateInfo(1).normalizedTime >= 0.7f)
+            {
+                Arrow.SetActive(false);
+            }
+            else
+            {
+                Arrow.SetActive(true);
+            }
+        }
+        else
+        {
+            Bow.SetActive(false);
+            Arrow.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.SetTrigger("Dead");
+        }
     }
     void LateUpdate()
     {
